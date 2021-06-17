549a9e4dcba286a2c560e5cee47f0e10a287b819
Shane Aldrich <81595892+s33a@users.noreply.github.com>
2021-06-17 13:35:28 -0700

    Added files via upload
    This is the beginning of my Materials module. This will include the taxonomy as well after I delete it from my local farmOS installation.

---
+++ src/Plugin/Asset/AssetType/Materials.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Drupal\farm_materials\Plugin\Asset\AssetType;
+
+use Drupal\farm_entity\Plugin\Asset\AssetType\FarmAssetType;
+
+/**
+ * Provides the compost asset type.
+ *
+ * @AssetType(
+ *   id = "materials",
+ *   label = @Translation("Materials"),
+ * )
+ */
+class Materials extends FarmAssetType {
+
+}
