f0c4622d5b917c43275cdd50ad02a017071e6bae
s33a <shane_aldrich@onmail.com>
2021-06-21 16:34:44 -0700

    Copied code from plant module to materials module for testing.

--- src/Plugin/Asset/AssetType/Materials.php
+++ src/Plugin/Asset/AssetType/Materials.php
@@ -14,4 +14,31 @@ use Drupal\farm_entity\Plugin\Asset\AssetType\FarmAssetType;
  */
 class Materials extends FarmAssetType {
 
+  /**
+   * {@inheritdoc}
+   */
+  public function buildFieldDefinitions() {
+    $fields = parent::buildFieldDefinitions();
+    $field_info = [
+      'material_type' => [
+        'type' => 'entity_reference',
+        'label' => $this->t('Substrates'),
+        'description' => "What Fungi species benefits from this substrate?",
+        'target_type' => 'taxonomy_term',
+        'target_bundle' => 'material_type',
+        'auto_create' => TRUE,
+        'required' => TRUE,
+        'multiple' => TRUE,
+        'weight' => [
+          'form' => -90,
+          'view' => -50,
+        ],
+      ],
+    ];
+    foreach ($field_info as $name => $info) {
+      $fields[$name] = $this->farmFieldFactory->bundleFieldDefinition($info);
+    }
+    return $fields;
+  }
+
 }
